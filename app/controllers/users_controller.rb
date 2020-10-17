44631e62fb85e48149822b3be1fff2f5b4bba9ed
github username <taimurmshah@gmail.com>
2020-10-17 19:54:47 -0400

    user resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,47 @@
 class UsersController < ApplicationController
 
-  # wrap_parameters :user, include: [:first_name, :last_name, :email,  :password]
+  def show
+    @user = User.find(params[:id])
+    render json:  @user
+  end
+
+  def index
+    @users = User.all
+    render json: @users
+  end
 
   def create
-    # byebug
     @user = User.new(user_params)
     puts @user
     if @user.valid?
       @user.save
-      render json: {user: @user}, status: :created
+      render json: {user: @user}, status: 201
     else
       render json: {errors: @user.errors.messages}
     end
   end
 
+  def update
+    id = params[:id]
+    @user = User.find(id)
+
+    @user.update(user_params)
+
+    render json: @user, status: 200
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render status: 200
+  end
+
+
   private
+
   #strong params
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:id, :first_name, :last_name, :email, :password)
   end
 
 end
